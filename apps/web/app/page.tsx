78f9c2ae446f096112790439db845e2857d24d19
PRAJWAL GHULE <prajwalghule2020@gmail.com>
2026-04-05 17:47:14 +0530

    cicd

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,6 +5,7 @@ export default async function Hone(){
   const user = await client.user.findFirst();
   return(
     <div>
+    <h1>Hello from cicd</h1>
       {user?.username}
       {user?.password}
     </div>
